1c1afa61cfe7b54a9af8d0876d9c5ec7d36498a8
Anjali Singh <105203288+Anjali0048@users.noreply.github.com>
2024-07-27 15:38:33 +0530

    added backend files

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+app.get("/", (req,res)=>{
+    res.send("Success")
+})
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server started at port : ${PORT}`)
